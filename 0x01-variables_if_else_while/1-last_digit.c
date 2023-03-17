e839a02e11f411175f4e915a52f929403fd54dba
wamburu <you@example.com>
2023-03-17 14:33:56 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,28 +2,22 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * maio -Determine if a number is positive, negative or zero
- *
- * Return : Always 0(Success)
+ * main prints the last digit of a randomly generated number
  */
-int main(void)
-{
+int main (void)
 	int n;
-
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-		else if (n == 01)
-		{
-			printf("%d is zero\n", n);
-		}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
+	n = rand() - RAND_MAX /2 ;
+       if ((n % 10) > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n,n %10);
 }
-
+else if ((n%10)<6 && (n%10)!=0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 8\n", n,n%10);
+	}
+else
+{
+	printf("Last digit of %d is %d and is 0\n", n,n %10);
+}
+return (0);
