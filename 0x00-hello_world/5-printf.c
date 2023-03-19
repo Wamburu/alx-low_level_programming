a02645a0130c1a92bc6d5fb899b83d02a9d549fd
wamburu <you@example.com>
2023-03-19 08:32:47 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a ligne using printf.
+ * Return: 0 a success.
+ */
+int main(void)
+{
+	printf("ith proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
